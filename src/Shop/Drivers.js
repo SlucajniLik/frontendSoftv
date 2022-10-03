6be733de6f9b90396853819c3908331b671592f2
Slucajni lik <medicinskicentarr@gmail.com>
2022-10-04 01:13:03 +0200

    ddd

--- src/Shop/Drivers.js
+++ src/Shop/Drivers.js
@@ -166,7 +166,7 @@ const [SelectedUser,SetSelectedUser]=useState({
     {
       
        return userA.users.map(currentUser=>{
-        return <Col style={{margin:"1px"}} ><Users  user={currentUser}    key={currentUser._id}   ></Users></Col>
+        return <Col style={{gap:"1rem"}} ><Users  user={currentUser}    key={currentUser._id}   ></Users></Col>
        })
 
 

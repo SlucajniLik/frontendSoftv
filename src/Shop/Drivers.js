45c16a49326674244654b80883a18ad3830c6729
Slucajni lik <medicinskicentarr@gmail.com>
2022-10-03 23:19:54 +0200

    dd

--- src/Shop/Drivers.js
+++ src/Shop/Drivers.js
@@ -274,7 +274,7 @@ padding: "10px",
 fontSize: "17px",
 border: "1px solid grey",
 float: "left",
-width: "30vh",
+width: "33vh",
 background: "#f1f1f1"
 
 }
@@ -285,7 +285,7 @@ background: "#f1f1f1"
   float:"left",
   width: "5vh",
   padding: "10px",
-  background: "#2196F3",
+  background: "#262626",
   color: "white",
   fontSize: "17px",
   border: "1px solid grey",

96b46f9fe70f528631fa2142a03a0371345bf4de
SlucajniLik <medicinskicentarr@gmail.com>
2022-10-04 22:15:37 +0200

    dd

--- src/Users/Footers.js
+++ src/Users/Footers.js
@@ -3,7 +3,7 @@ import React from 'react'
  function Footers() {
   return (
     <div  className="bg-dark"  style={{color:'white',height:'5vh',textAlign:'center',padding:'1vh'}}>
-    <p>Moja firma</p>
+    <p>My-Company</p>
   </div>
   )
 }

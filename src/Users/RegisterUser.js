90da37fffc29423088b6b3b29d01442bc486a3b9
SlucajniLik <medicinskicentarr@gmail.com>
2022-10-04 19:22:44 +0200

    dd

--- src/Users/RegisterUser.js
+++ src/Users/RegisterUser.js
@@ -417,9 +417,9 @@ else
 return(
   <Container 
   className="d-flex align-items-center justify-content-center"
-  style={{ minHeight: "40vh"}}
+  style={{ minHeight: "90vh"}}
 >
-  <div className="w-100" style={{ maxWidth: "400px" }}>
+  <div className="w-100" style={{ maxWidth: "400px",background:"red"}}>
 
 
       <Card>

d16673647d97665e0c33ba15c4cbcff9f7818f94
Slucajni lik <medicinskicentarr@gmail.com>
2022-10-04 13:20:05 +0200

    dd

--- src/Admin/UsersApp.js
+++ src/Admin/UsersApp.js
@@ -163,7 +163,28 @@ if(userA.users.length>0)
     <>
      <br/>
      <form  style={{backgrond:"red",width:"100%",display:"flex",alignItems:"center",justifyContent:"center"}}  >
-   
+      <input type="text"     onChange={onChangeSearch}   value={SelectedUser.name}  placeholder="Pretraga vozaca po imenu ili prezimenu" name="search"/>
+      <button type="submit"   onClick={SearchUser}   style={
+
+{
+  float:"left",
+  width: "5vh",
+  padding: "10px",
+  background: "#262626",
+  color: "white",
+  fontSize: "17px",
+  border: "1px solid grey",
+  borderLeft: "none",
+  cursor: "pointer",
+
+
+
+
+
+
+}
+
+      }    ><i class="fa fa-search"></i></button>
     </form>
 <br/>
   <Container 

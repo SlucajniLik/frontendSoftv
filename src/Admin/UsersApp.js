7739296cc675adce2975a3b56c331d9c2f20c1d7
Slucajni lik <medicinskicentarr@gmail.com>
2022-10-04 02:41:11 +0200

    ddd

--- src/Admin/UsersApp.js
+++ src/Admin/UsersApp.js
@@ -160,53 +160,6 @@ if(userA.users.length>0)
 {
   return (
   
-    <>
-     
-     <form >
-      <input type="text"  style={
-{
-padding: "10px",
-fontSize: "17px",
-border: "1px solid grey",
-float: "left",
-width: "33vh",
-background: "#f1f1f1"
-
-}
-      }     onChange={onChangeSearch}   value={SelectedUser.name}  placeholder="Pretraga vozaca po imenu ili prezimenu" name="search"/>
-      <button type="submit"   onClick={SearchUser}   style={
-
-{
-  float:"left",
-  width: "5vh",
-  padding: "10px",
-  background: "#262626",
-  color: "white",
-  fontSize: "17px",
-  border: "1px solid grey",
-  borderLeft: "none",
-  cursor: "pointer",
-
-
-
-
-
-
-}
-
-      }    ><i class="fa fa-search"></i></button>
-    </form>
-
-  <Container 
-  
-  className="d-flex  justify-content-center h-100 "
-  style={{ minHeight: "90vh",minWidth:'150vh',marginTop:"50px"}}
->
-
-
-
-  
-<br/>
 
     <Table  striped bordered hover>
           <thead>
@@ -229,8 +182,8 @@ background: "#f1f1f1"
         </Table>
      
     
-    </Container>
-    </>
+
+
     
     
       )

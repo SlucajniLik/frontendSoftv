3a52b381bda9bf0c24a091a006869da93896869a
SlucajniLik <medicinskicentarr@gmail.com>
2022-10-05 02:10:03 +0200

    ss

--- src/Users/RegisterUser.js
+++ src/Users/RegisterUser.js
@@ -453,13 +453,14 @@ return(
       <Form.Group className="mb-3" controlId="formBasicPassword">
         <Form.Label>Lozinka</Form.Label>
         <Form.Control type={passwordShown ?"text":"password"} placeholder="Unesite vasu lozinku" value={password} onChange={onChangePass}    />
-        <Form.Control type="checkbox" onClick={togglePassword}>Prikazi lozinku</Form.Control>
+        <label><input type="checkbox" onClick={togglePassword}  value=""/>Prikazi lozinku</label>
         {!passwordErr&& <p   style={{color:'red'}}   >Vasa sifra mora sadrzati 8 karaktera najmanje</p>}
       </Form.Group>
       <Form.Group className="mb-3" controlId="formConfirmPassword">
         <Form.Label>Potvrda lozinke</Form.Label>
         <Form.Control type="password" placeholder="Potvrdite vasu lozinku"     value={password2} onChange={onChangePass2}      />
-        <Form.Control type="checkbox" onClick={togglePassword}>Prikazi lozinku</Form.Control>
+        <label><input type="checkbox" onClick={togglePassword}  value=""/>Prikazi lozinku</label>
+   
         {!password2Err && <p   style={{color:'red'}}   >Uneli ste pogresnu sifru</p>}
       </Form.Group>
       <Form.Group controlId="formFile" className="mb-3">

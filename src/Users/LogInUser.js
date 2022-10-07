20c210e1f17fce06b1d597c84618e3f95e8d1c0f
Slucajni lik <medicinskicentarr@gmail.com>
2022-10-07 14:31:42 +0200

    ddd

--- src/Users/LogInUser.js
+++ src/Users/LogInUser.js
@@ -4,7 +4,8 @@ import Button from 'react-bootstrap/Button';
 import {Form,Card,Container} from 'react-bootstrap';
 import Cookies from 'js-cookie';
 import jwt_decode from "jwt-decode";
-import FontAwesomeIcon from '@fortawesome/react-fontawesome';
+import FontAwesomeIcon from "@fortawesome/react-fontawesome"
+import * as Icons from "@fortawesome/fontawesome-free-solid"
 import { DefContext } from "../Helpers/DefContext";
 import { useNavigate,Link,useParams } from "react-router-dom";
 import { faEye } from "@fortawesome/free-solid-svg-icons";

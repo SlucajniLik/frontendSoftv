01416b6e734a107a7ddf62451b92107783c04b9a
Slucajni lik <medicinskicentarr@gmail.com>
2022-10-18 15:28:53 +0200

    ddd

--- src/Navbar.js
+++ src/Navbar.js
@@ -77,7 +77,7 @@ function Navbarr() {
 
 }
                       
-{userState.status &&<Nav.Link    ><Button className='loggedInContainer'    onClick={logout} name="logOut"  > Logout</Button></Nav.Link>}      
+{userState.status && <><Nav.Link    ><Button className='loggedInContainer'    onClick={logout} name="logOut"  > Logout</Button></Nav.Link></>}      
           </Nav>
      
           </Navbar.Collapse>

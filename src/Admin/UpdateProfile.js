e2079ab485ecc48eaf7de34ee02cffe35f825f28
Slucajni lik <medicinskicentarr@gmail.com>
2022-10-20 11:34:43 +0200

    ddd

--- src/Admin/UpdateProfile.js
+++ src/Admin/UpdateProfile.js
@@ -199,12 +199,12 @@ function onSubmit(e)
       image:image
   }*/
 
-  const user=new FormData()
+  /*const user=new FormData()
 
 
   user.append("email",email)
   user.append("password",password)
-  user.append("image",image)
+  user.append("image",image)*/
 
 //console.log(validate(name,surname,email,password,password2,role))
 

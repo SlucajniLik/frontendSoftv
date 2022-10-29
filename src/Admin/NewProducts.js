e9904f081bd7fd33d5ea62d558a4454e9b05aba8
SlucajniLik <medicinskicentarr@gmail.com>
2022-10-29 15:05:55 +0200

    dd

--- src/Admin/NewProducts.js
+++ src/Admin/NewProducts.js
@@ -180,6 +180,7 @@ return error
       <Form.Group controlId="formFile" className="mb-3">
         <Form.Label>Unesite sliku:</Form.Label>
         <Form.Control  ref={refImage}  type="file" filename="image"   onChange={onChangeImage}/>
+        <p   style={{color:'blue'}}   >Slika je obavezna</p>
         {!imageErr && <p   style={{color:'red'}}   >Unesite sliku proizvoda</p>}
         {Success && <p   style={{color:'green'}}   >Uspesno ste uneli proizvod</p>}
       </Form.Group>
